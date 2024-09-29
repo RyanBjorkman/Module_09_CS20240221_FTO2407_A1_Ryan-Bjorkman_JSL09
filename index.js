d6d9dc22340f3708c1edfba56be4d4e9d72df944
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-09-29 13:59:05 +0200

    created crypto corner with stats

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     });
 
     fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => res.json())
-    .then(data => console.log(data))
+    .then(res => {
+        if(!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+            <img src="${data.image.small}"/>
+            <span>${data.name}</span>
+        `
+        document.getElementById("crypto").innerHTML += `
+                    <p>🎯 $${data.market_data.current_price.usd}</p>
+                    <p>⬆️ $${data.market_data.high_24h.usd}</p>
+                    <p>⬇️ $${data.market_data.low_24h.usd}</p>
+        `
+    })
+
     .catch(err => console.error(err))
