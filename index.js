ec0243cabc3adf705647720761e421f66a0fa7cc
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-09-29 13:16:50 +0200

    deleted redundant scrimba files

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
+        document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+    .catch(() => {
+        // set a default background image if the fetch fails
+         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+    });
 
+    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
